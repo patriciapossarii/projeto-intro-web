2bbdf25e43f865316ed348b68453b1fdb594a6c9
patriciapossarii <patricia.mag.carvalho@gmail.com>
2022-09-13 08:01:54 -0300

    Adicionando versão do projeto - Semana 2

--- index.js
+++ index.js
@@ -1,61 +1,58 @@
-const motorcycleModel1 = "F800"
-const motorcycleBrand1 = "BMW"
-const motorcycleManufactureYear1 = 2015
-const motorcycleEngineCapacity1 = 798
-const motorcycleFlex1 = false
-const motorcycleDetails1 = ["Azul","Corrente","Partida Elétrica", "Peso 207 Kg"]
+const objeto1 = {
+    motorcycleModel: "F800",
+    motorcycleBrand: "BMW",
+    motorcycleManufactureYear: 2015,
+    motorcycleEngineCapacity1: 798,
+    motorcycleDocumentationIsOK: true,
+    motorcycleDetails: ["Azul", "Corrente", "Partida Elétrica", "Peso 207 Kg"],
+}
 
-const motorcycleModel2 = "Tiger 800"
-const motorcycleBrand2 = "Triumph"
-const motorcycleManufactureYear2 = 2019
-const motorcycleEngineCapacity2 = 800
-const motorcycleFlex2 = false
-const motorcycleDetails2 = ["Preto", "Corrente", "Partida Eletrica", "Peso 213 Kg"]
+const objeto2 = {
+    motorcycleModel: "Tiger 800",
+    motorcycleBrand: "Triumph",
+    motorcycleManufactureYear: 2019,
+    motorcycleEngineCapacity: 800,
+    motorcycleDocumentationIsOK: false,
+    motorcycleDetails: ["Preto", "Corrente", "Partida Eletrica", "Peso 213 Kg"],
+}
 
-const motorcycleModel3 = "V-Rod"
-const motorcycleBrand3 = "Harley Davidson"
-const motorcycleManufactureYear3 = 2016
-const motorcycleEngineCapacity3 = 1250
-const motorcycleFlex3 = false
-const motorcycleDetails3 = ["Laranja", "Peso 303"]
+const objeto3 = {
+    motorcycleModel: "V-Rod",
+    motorcycleBrand: "Harley Davidson",
+    motorcycleManufactureYear: 2016,
+    motorcycleEngineCapacity: 1250,
+    motorcycleDocumentationIsOK: true,
+    motorcycleDetails: ["Laranja", "Peso 303"],
+}
 
-const averageManufactureYear = (motorcycleManufactureYear1+motorcycleManufactureYear2+motorcycleManufactureYear3)/3
-const checkFlex = motorcycleFlex1 && motorcycleFlex2 && motorcycleFlex3
+const averageManufactureYear = (objeto1.motorcycleManufactureYear + objeto2.motorcycleManufactureYear + objeto3.motorcycleManufactureYear) / 3
 
-console.log(`
-Modelo: ${motorcycleModel1.toUpperCase()}
-Fabricante: ${motorcycleBrand1}
-Ano de Fabricação: ${motorcycleManufactureYear1}
-Cilindrada: ${motorcycleEngineCapacity1}
-Flex: ${motorcycleFlex1}
-Detalhes: ${motorcycleDetails1}
-`)
+armazenaObjetos = []
 
-console.log(`
-Modelo: ${motorcycleModel2.toLocaleUpperCase()}
-Fabricante: ${motorcycleBrand2}
-Ano de Fabricação: ${motorcycleManufactureYear2}
-Cilindrada: ${motorcycleEngineCapacity2}
-Flex: ${motorcycleFlex2}
-Detalhes: ${motorcycleDetails2}
-`)
+if (objeto1.motorcycleDocumentationIsOK === true) {
+    armazenaObjetos.push(objeto1)
+    console.log("Item adicionado com sucesso!")
+}
+else {
+    alert("Item NÃO adicionado! Favor verificar com o setor de cadastro")
+}
+if (objeto2.motorcycleDocumentationIsOK === true) {
+    armazenaObjetos.push(objeto2)
+    console.log("Item adicionado com sucesso!")
+}
+else {
+    alert("Item NÃO adicionado! Favor verificar com o setor de cadastro")
+}
+if (objeto3.motorcycleDocumentationIsOK === true) {
+    armazenaObjetos.push(objeto3)
+    console.log("Item adicionado com sucesso!")
+}
+else {
+    alert("Item NÃO adicionado! Favor verificar com o setor de cadastro")
+}
 
-console.log(`
-Modelo: ${motorcycleModel3.toLocaleUpperCase()}
-Fabricante: ${motorcycleBrand3}
-Ano de Fabricação: ${motorcycleManufactureYear3}
-Cilindrada: ${motorcycleEngineCapacity3}
-Flex: ${motorcycleFlex3}
-Detalhes: ${motorcycleDetails3}
-`)
+console.log(armazenaObjetos)
 
 console.log(`
 A média do ano das motos é: ${Math.round(averageManufactureYear)}
-Contém motos flex: ${checkFlex}
 `)
-
-
-
-
-
-
