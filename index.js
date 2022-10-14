b0a21f981a319d3b1b6b380b5e779cc815ee5178
patriciapossarii <patricia.mag.carvalho@gmail.com>
2022-10-14 19:58:30 -0300

    Atualizando semana 6 - Botão de busca

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+// ============================ BANCO DE DADOS DAS MOTOS ============================
 const objeto0 = {
     motorcycleModel: "F9900",
     motorcycleBrand: "BMW",
@@ -35,73 +37,46 @@ const objeto3 = {
     motorcycleEngineCapacity: 1250,
     motorcycleDocumentationIsOK: true,
     motorcycleDetails: ["Laranja", "Peso 303"],
-    motorcyclePhoto: "v-road.jpg",
+    motorcyclePhoto: "/Assets/v-road.jpg",
 }
 
-const averageManufactureYear = (objeto1.motorcycleManufactureYear + objeto2.motorcycleManufactureYear + objeto3.motorcycleManufactureYear) / 3
 
+// ============================ MÉDIA DE ANO DAS MOTOS ============================
+const averageManufactureYear = (objeto1.motorcycleManufactureYear + objeto2.motorcycleManufactureYear + objeto3.motorcycleManufactureYear) / 3
+console.log(`A média do ano das motos é: ${Math.round(averageManufactureYear)}`)
+
+
+// ============================ ARMAZENA MOTOS COM OS DOCUMENTOS OK ============================
 armazenaObjetos = []
 if(objeto0.motorcycleDocumentationIsOK === true) {
     armazenaObjetos.push(objeto0)
     console.log("Item adicionado com sucesso!")
 }
-else {
-    alert("Item NÃO adicionado! Favor verificar com o setor de cadastro")
-}
+
 
 if (objeto1.motorcycleDocumentationIsOK === true) {
     armazenaObjetos.push(objeto1)
     console.log("Item adicionado com sucesso!")
 }
-else {
-    alert("Item NÃO adicionado! Favor verificar com o setor de cadastro")
-}
+
 if (objeto2.motorcycleDocumentationIsOK === true) {
     armazenaObjetos.push(objeto2)
     console.log("Item adicionado com sucesso!")
 }
-else {
-    alert("Item NÃO adicionado! Favor verificar com o setor de cadastro")
-}
+
 if (objeto3.motorcycleDocumentationIsOK === true) {
     armazenaObjetos.push(objeto3)
     console.log("Item adicionado com sucesso!")
-    alert("Item NÃO adicionado! Favor verificar com o setor de cadastro")
-}
+   }
+
 for (i = 0; i <= armazenaObjetos.length; i++) {
     console.log("Itens adicionados com sucesso-!:", armazenaObjetos[i])
 }
-console.log(`================= SEMANA 3 - ITEM 1 E 2 ===========================`)
-
-const retornaArmazenaObjetos = (motos) => {
-    for (let i of motos) {
-        console.log(`${i.motorcycleModel.toUpperCase()}
-    Modelo: ${i.motorcycleBrand}
-    Marca: ${i.motorcycleManufactureYear}
-    Capacidade do motor: ${i.motorcycleEngineCapacity}
-    A documentação estpa OK? ${i.motorcycleDocumentationIsOK}
-    Detalhes: ${i.motorcycleDetails.toString()}`)
-        return retornaArmazenaObjetos
-    }
-}
-
-retornaArmazenaObjetos(armazenaObjetos)
-
-
-console.log(`================= SEMANA 3 - ITEM 3 ===========================`)
-
-const objetoParaString = (moto) => {
-    let transformaString = moto.motorcycleModel + " " + moto.motorcycleBrand + " " + moto.motorcycleManufactureYear + " " + moto.motorcycleEngineCapacity + " " + moto.motorcycleDocumentationIsOK + " " + moto.motorcycleDetails
-    console.log("Retona o Objeto como String: ", transformaString)
-}
-objetoParaString(objeto1)
-
-console.log(`================= SEMANA 3 - ITEM 4 ===========================`)
-
+// ============================ FILTRA MARCA DE MOTOS ============================
 
 const filtraMarca = (recObjeto, recString) => {
     const retornaObjeto = recObjeto.filter((brand) => {
-        return brand.motorcycleBrand === recString
+        return brand.motorcycleBrand.toUpperCase() === recString
 
     })
     return retornaObjeto
@@ -111,46 +86,38 @@ const filtro = filtraMarca(armazenaObjetos, "BMW")
 filtro.length != 0 ? console.log("Marca Filtrada", filtro) : alert("O item pesquisado nao existe")
 
 
-
-console.log(`============================================`)
-
-
-/*
------
------
-${objeto3.motorcycleModel.toUpperCase()}
-Modelo: ${objeto3.motorcycleBrand}
-Marca: ${objeto3.motorcycleManufactureYear}
-Capacidade do motor: ${objeto3.motorcycleEngineCapacity}
-A documentação estpa OK? ${objeto3.motorcycleDocumentationIsOK}
-Detalhes: ${objeto3.motorcycleDetails}
-`)
-*/
-
-console.log(`============================================`)
-
-console.log(`A média do ano das motos é: ${Math.round(averageManufactureYear)}`)
-
-
 //=========== DOM =========== DOM =========== DOM =========== DOM =========== DOM ===========
 
 function generateListItems(arg) {
     let items = ''
     for (let i = 0; i < arg.length; i++) {
-        items += `<section class="mc"><img src=${arg[i].motorcyclePhoto}><ul> `;
-        items += ` ${arg[i].motorcycleBrand}`;
-        items += `<li> Modelo:  ${arg[i].motorcycleModel}</li>`;
-        items += `<li> Ano de Fabricação: ${arg[i].motorcycleManufactureYear}</li>`;
-        items += `<li> Capacidade do Motor: ${arg[i].motorcycleEngineCapacity} </li>`;
-        items += `<li> Detalhes: ${arg[i].motorcycleDetails.toString()}</li></ul></section>`;
-        
+        items += `<div class="mc"><img src=${arg[i].motorcyclePhoto}>`;
+        items += ` <ul><p>${arg[i].motorcycleBrand}</p>`;
+        items += `<p>Modelo:  ${arg[i].motorcycleModel}</p>`;
+        items += `<p>Ano de Fabricação: ${arg[i].motorcycleManufactureYear}</p>`;
+        items += `<p>Capacidade do Motor: ${arg[i].motorcycleEngineCapacity} </p>`;
+        items += `<p>Detalhes: ${arg[i].motorcycleDetails.toString()}</p></ul></div>`;
         console.log("passei")
     }
     return items;
 
 }
 
+// ============================ BUSCA ============================
 function insereItem(event) {
-    document.getElementById("lista").innerHTML = generateListItems(filtro)
-    input.value = "" //extra
-}
+    const inputUser = document.getElementById("meu-input").value.toUpperCase();
+    console.log(inputUser)
+    const filtroInputUser = filtraMarca(armazenaObjetos, inputUser)
+    document.getElementById("lista").innerHTML = generateListItems(filtroInputUser)
+    inputUser.value = ""
+   }
+
+// ============================ LIMPAR BUSCA ============================
+function limparBusca(event) {
+    document.getElementById("lista").innerHTML = generateListItems(armazenaObjetos)
+    inputUser.value = ""
+   }
+
+
+// ============================ MOTOS TELA É CARREGADA (INDEX) ============================
+document.getElementById("lista").innerHTML = generateListItems(armazenaObjetos)
