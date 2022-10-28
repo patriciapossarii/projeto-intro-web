f032716f88770a891d74847fef9370e82f1f80b6
patriciapossarii <patricia.mag.carvalho@gmail.com>
2022-10-28 07:27:20 -0300

    Atualizando semana 6 - Incuindo mais objetos e calculando a média de km das motos, responsividade do menu e páginas dos menus

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 
 // ============================ BANCO DE DADOS DAS MOTOS ============================
 const objeto0 = {
-    motorcycleModel: "F9900",
-    motorcycleBrand: "BMW",
-    motorcycleManufactureYear: 2015,
-    motorcycleEngineCapacity: 798,
+    motorcycleModel: "CLASSIC 350",
+    motorcycleBrand: "Royal Enfield",
+    motorcycleManufactureYear: 2021,
+    motorcycleEngineCapacity: 249,
     motorcycleDocumentationIsOK: true,
-    motorcycleDetails: ["Azul", "Corrente", "Partida Elétrica", "Peso 207 Kg"],
-    motorcyclePhoto: "/Assets/f800.jpg",
+    motorcycleDetails: ["Verde", "Corrente", "Partida Elétrica", "Peso 195 Kg"],
+    motorcyclePhoto: "/Assets/royal.jpg",
 }
 
 const objeto1 = {
@@ -40,37 +40,50 @@ const objeto3 = {
     motorcyclePhoto: "/Assets/v-road.jpg",
 }
 
+const objeto4 = {
+    motorcycleModel: "1290  Super Duke R 2021",
+    motorcycleBrand: "KTM",
+    motorcycleManufactureYear: 2022,
+    motorcycleEngineCapacity: 1301,
+    motorcycleDocumentationIsOK: true,
+    motorcycleDetails: ["Laranja e Branco", "Peso 189"],
+    motorcyclePhoto: "/Assets/1290.png",
+}
 
 // ============================ MÉDIA DE ANO DAS MOTOS ============================
-const averageManufactureYear = (objeto1.motorcycleManufactureYear + objeto2.motorcycleManufactureYear + objeto3.motorcycleManufactureYear) / 3
+const averageManufactureYear = (objeto0.motorcycleManufactureYear + objeto1.motorcycleManufactureYear + objeto2.motorcycleManufactureYear + objeto3.motorcycleManufactureYear + objeto4.motorcycleManufactureYear) / 5
 console.log(`A média do ano das motos é: ${Math.round(averageManufactureYear)}`)
 
 
 // ============================ ARMAZENA MOTOS COM OS DOCUMENTOS OK ============================
 armazenaObjetos = []
+
 if(objeto0.motorcycleDocumentationIsOK === true) {
     armazenaObjetos.push(objeto0)
-    console.log("Item adicionado com sucesso!")
+    console.log("Item adicionado com sucesso! Modelo:",objeto0.motorcycleModel)
 }
-
 
 if (objeto1.motorcycleDocumentationIsOK === true) {
     armazenaObjetos.push(objeto1)
-    console.log("Item adicionado com sucesso!")
+    console.log("Item adicionado com sucesso! Modelo:",objeto1.motorcycleModel)
 }
 
 if (objeto2.motorcycleDocumentationIsOK === true) {
     armazenaObjetos.push(objeto2)
-    console.log("Item adicionado com sucesso!")
+    console.log("Item adicionado com sucesso! Modelo:",objeto2.motorcycleModel)
 }
 
 if (objeto3.motorcycleDocumentationIsOK === true) {
     armazenaObjetos.push(objeto3)
-    console.log("Item adicionado com sucesso!")
+    console.log("Item adicionado com sucesso! Modelo:",objeto3.motorcycleModel)
    }
 
+   if (objeto4.motorcycleDocumentationIsOK === true) {
+    armazenaObjetos.push(objeto4)
+    console.log("Item adicionado com sucesso! Modelo:",objeto3.motorcycleModel)
+   }
 for (i = 0; i <= armazenaObjetos.length; i++) {
-    console.log("Itens adicionados com sucesso-!:", armazenaObjetos[i])
+    console.log("Motocicletas armazenadas com os documentos ok: ", armazenaObjetos[i])
 }
 // ============================ FILTRA MARCA DE MOTOS ============================
 
@@ -108,13 +121,14 @@ function insereItem(event) {
     const inputUser = document.getElementById("meu-input").value.toUpperCase();
     console.log(inputUser)
     const filtroInputUser = filtraMarca(armazenaObjetos, inputUser)
-    document.getElementById("lista").innerHTML = generateListItems(filtroInputUser)
+    filtroInputUser.length !=0?  document.getElementById("lista").innerHTML = generateListItems(filtroInputUser): alert("Item não encontrado")
     inputUser.value = ""
    }
 
 // ============================ LIMPAR BUSCA ============================
 function limparBusca(event) {
     document.getElementById("lista").innerHTML = generateListItems(armazenaObjetos)
+    const inputUser = document.getElementById("meu-input")
     inputUser.value = ""
    }
 
